0f318e8a8bcc790dd01e389d50fe68e9dad35ee9
unknown <keonp2@illinois.edu>
2017-12-22 19:24:23 -0500

    modified flask test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, session
 import json
+import pprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'half-life 3 confirmed'
@@ -10,7 +11,10 @@ def receiver():
     if request.is_json:
         payload = request.get_json()
         content = json.loads(payload)
-        return content
+        pprint.pprint(content)
+        return 'JSON Posted'
+    else:
+        return 'POST request was not in json'
 
 
 if __name__ == "__main__":
