14986f5167a191fc1d876e524b804b5200c6c56a
unknown <keonp2@illinois.edu>
2017-12-27 03:17:45 -0500

    WIP starter and ender functionality to trigger start and end data collection from player

--- main.py
+++ main.py
@@ -28,29 +28,35 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-# the to-be frontend
+# the frontend
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        conn = get_db()
-        value = str(request.form.get('choose'))
-        result = None
-        if value == 'match':
-            result = query_db_match(conn)
-        if value == 'today':
-            result = query_db_today(conn)
-        if value == 'week':
-            result = query_db_week(conn)
-        if value == 'month':
-            result = query_db_month(conn)
-        if value == 'lifetime':
-            result = query_db_lifetime(conn)
-        if result is None:
-            return render_template('index.html')
+    if request.method == 'POST':  # TODO: add 'ender' request.form handler to set app.config['STARTER'] to False and end collection.
+        starter = str(request.form.get('starter'))
+        if starter == 'starter':
+            app.config['STARTER'] = True
+            return redirect(url_for('index'))
         else:
-            session['result'] = result
-            return redirect(url_for('results'))
+            conn = get_db()
+            value = str(request.form.get('choose'))
+            result = None
+            if value == 'match':
+                result = query_db_match(conn)
+            if value == 'today':
+                result = query_db_today(conn)
+            if value == 'week':
+                result = query_db_week(conn)
+            if value == 'month':
+                result = query_db_month(conn)
+            if value == 'lifetime':
+                result = query_db_lifetime(conn)
+            if result is None:
+                return redirect(url_for('index'))
+            else:
+                session['result'] = result
+                return redirect(url_for('results'))
     else:
+        print(app.config['STARTER'])
         return render_template('index.html')
 
 
@@ -68,7 +74,7 @@ def docs():
 # backend POST request handler
 @app.route('/GS', methods=['POST'])
 def GSHandler():
-    if request.is_json:
+    if request.is_json and app.config['STARTER']:
         payload = request.get_json()
         conn = get_db()
         counter = check_payload(payload)
@@ -82,10 +88,11 @@ def GSHandler():
             stats_df.to_sql("per_round_data", conn, if_exists="append", index=False)
             clean_db(conn)
             print(pd.read_sql('select * from per_round_data;', conn))
-
+    print(app.config['STARTER'])
     return 'JSON Posted'
 
 
 if __name__ == "__main__":
     webbrowser.open_new('http://127.0.0.1:5000')  # for deployment
+    app.config['STARTER'] = False  # starter variable
     app.run(debug=False)
