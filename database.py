e7dfd341cdd06861a53a22351ac3a2551d511425
df df <keonp2@illinois.edu>
2017-12-28 00:46:38 -0500

    more data

--- database.py
+++ database.py
@@ -9,17 +9,17 @@ pd.set_option('display.width', 1000)
 
 conn = sqlite3.connect('player_data.db')
 data_df = pd.read_sql('SELECT * FROM per_round_data;', conn)
+print(data_df)
 
-
-# selecting last match
-idx_range = data_df[data_df['Map Status'] == 'gameover'].index.tolist()[-2:]
-length = len(idx_range)
-if length < 2:
-    if length == 1:  # there has only been one 'gameover' event.
-        data_df = data_df.iloc[:idx_range[0] + 1]
-    else:
-        pass  # return query_db_time(conn, 'lifetime') because there have been no gameovers ever.
-else:
-    data_df = data_df.iloc[idx_range[0] + 1:idx_range[1] + 1]
-    print(data_df)
-    print(int(data_df['Current Equip. Value'].mean()))
+# # selecting last match
+# idx_range = data_df[data_df['Map Status'] == 'gameover'].index.tolist()[-2:]
+# length = len(idx_range)
+# if length < 2:
+#     if length == 1:  # there has only been one 'gameover' event.
+#         data_df = data_df.iloc[:idx_range[0] + 1]
+#     else:
+#         pass  # return query_db_time(conn, 'lifetime') because there have been no gameovers ever.
+# else:
+#     data_df = data_df.iloc[idx_range[0] + 1:idx_range[1] + 1]
+#     print(data_df)
+#     print(int(data_df['Current Equip. Value'].mean()))
