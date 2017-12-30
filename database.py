7fc996b4b5947a8b987da38a044506033d39285d
unknown <keonp2@illinois.edu>
2017-12-29 21:20:54 -0500

    KDA finished, now going to transfer to run.py

--- database.py
+++ database.py
@@ -11,8 +11,8 @@ conn = sqlite3.connect('player_data.db')
 data_df = pd.read_sql('SELECT * FROM per_round_data;', conn)
 # data_df = data_df.drop([x for x in range(20, 49)])
 # data_df = data_df.reset_index(drop=True)
-print(data_df)
-print('\n')
+# # print(data_df)
+# # print('\n')
 
 df_list = []
 idx_range = sorted(set([0] + data_df[data_df['Map Status'] == 'gameover'].index.tolist() + [data_df.index[-1]]))
@@ -28,28 +28,31 @@ if len(idx_range) == 1:
     df_list = [data_df]
 
 total_kills = 0
+total_assists = 0
 total_deaths = 0
 
 for match_df in df_list:
-    # print(match_df)
-    # print('\n')
+    print(match_df)
+    print('\n')
     if match_df.iloc[-1]['Player Name'] is None and match_df.iloc[-1]['Player Team'] is None and math.isnan(match_df.iloc[-1]['Score']):
         try:
             max_df = match_df.iloc[-2]
         except IndexError:
-            max_df = match_df.iloc[-1]
+            max_df = match_df.iloc[-1].fillna(0)
     else:
         max_df = match_df.iloc[-1]
-    # print(max_df)
     total_kills += int(max_df['Kills'])
+    total_assists += int(max_df['Assists'])
     total_deaths += int(max_df['Deaths'])
-    # print('\n')
 
 print('\n')
 print(total_kills)
+print(total_assists)
 print(total_deaths)
 
 total_kdr = float(round(total_kills / total_deaths, 3))
-print(total_kdr)
+total_kda = float(round((total_kills + total_assists) / total_deaths, 3))
+print('KDR: %s' % total_kdr)
+print('KDA: %s' % total_kda)
 
 
