32df4dcca3aeb73914b9809dc5ea46ede42a7061
unknown <keonp2@illinois.edu>
2017-12-30 22:26:46 -0500

    WIP round count per map graph

--- database.py
+++ database.py
@@ -2,6 +2,8 @@
 
 import sqlite3
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from run import *
 
@@ -16,8 +18,6 @@ df_list = separate(data_df)
 map_list = list(set(data_df['Map'].tolist()))
 map_list = [x for x in map_list if x != 'RESET POINT']
 
-print(map_list)
-
 round_count_dict = dict.fromkeys(map_list, 0)
 
 for df in df_list:
@@ -26,3 +26,12 @@ for df in df_list:
         round_count_dict[cs_map] += len(df[df['Map'] == cs_map].index)
 
 print(round_count_dict)
+
+fig = plt.figure()
+plt.bar(range(len(round_count_dict)), list(round_count_dict.values()), align='center')
+plt.xticks(range(len(round_count_dict)), list(round_count_dict.keys()))
+fig.suptitle('Rounds Played by Map')
+plt.xlabel('Map')
+plt.ylabel('Count')
+
+plt.savefig('templates/rounds_per_map.png')
