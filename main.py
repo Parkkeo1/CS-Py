d1705b24acf92bf547dc5298c3035d31fba8bb3d
unknown <keonp2@illinois.edu>
2018-01-13 03:02:39 -0500

    switching to remove/edit method

--- main.py
+++ main.py
@@ -137,8 +137,11 @@ def GSHandler():
             print(stats_df)
             print('\n')
             last_df = pd.read_sql('SELECT * FROM per_round_data ORDER BY Time DESC LIMIT 1;', conn)
-            if counter == 2:  # makes sure that, if we are attempting to enter in an endgame-stats_df, check that the current last entry in the table is not also a 'gameover' entry.
-                if len(last_df.index) == 0 or last_df.iloc[0]['Map Status'] != 'gameover':  # this prevents having two 'gameover' events in a row, where the latter is a None/NaN entry.
+            if counter == 2:
+                # makes sure that, if we are attempting to enter in an endgame-stats_df,
+                # check that the current last entry in the table is not also a 'gameover' entry.
+                # This prevents having two 'gameover' events in a row, where the latter is a None/NaN entry.
+                if len(last_df.index) == 0 or last_df.iloc[0]['Map Status'] != 'gameover':
                     stats_df.to_sql("per_round_data", conn, if_exists="append", index=False)
                     clean_db(conn)
                     print('successful 2')
@@ -148,15 +151,26 @@ def GSHandler():
                     clean_db(conn)
                     print('successful 1')
                 else:  # time difference is 1 second or less
-                    if stats_df.iloc[0]['Map Status'] == 'gameover' and last_df.iloc[0]['Map Status'] == 'live':
-                        # TODO: delete last_df's entry/row and replace it with stats_df entry.
-                        # To prevent cases such as this:
-                        #
-                        # 52  1515805253  de_cache       live   dumby  T  23.0  3.0  17.0  3.0  56.0  5000.0   1.0   0.0
-                        # 53  1515805254  de_cache   gameover   dumby  T  23.0  4.0  17.0  3.0  57.0  5000.0   1.0   0.0
-                        # stats_df.to_sql("per_round_data", conn, if_exists="append", index=False)
-                        # clean_db(conn)
-                        pass
+                    # if stats_df.iloc[0]['Map Status'] == 'gameover' and last_df.iloc[0]['Map Status'] == 'live':
+                    #     # TODO: delete last_df's entry/row and replace it with stats_df entry.
+                    #     # To prevent cases such as this:
+                    #     #
+                    #     # 52  1515805253  de_cache       live   dumby  T  23.0  3.0  17.0  3.0  56.0  5000.0  1.0  0.0
+                    #     # 53  1515805254  de_cache   gameover   dumby  T  23.0  4.0  17.0  3.0  57.0  5000.0  1.0  0.0
+                    #     # stats_df.to_sql("per_round_data", conn, if_exists="append", index=False)
+                    #     # clean_db(conn)
+                    #     pass
+
+                    # WIP
+                    total_df = pd.read_sql('SELECT * FROM per_round_data', conn)
+                    new_df = total_df.iloc[:-1]  # removing last entry AKA last_df
+                    new_df.to_sql("per_round_data", conn, if_exists="replace", index=False)
+                    
+                    # Inserting new entry, stats_df
+                    stats_df.to_sql("per_round_data", conn, if_exists="append", index=False)
+                    clean_db(conn)
+                    print('successful 1')
+
     return 'JSON Posted'
 
 
