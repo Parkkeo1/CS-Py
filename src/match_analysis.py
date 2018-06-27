0f5bfcd78fd2ebc031b34d3fc2a1d3cc0b62c13b
Parkkeo1 <keonp2@illinois.edu>
2018-06-27 17:42:50 -0400

    structure for stats analysis almost complete

--- src/match_analysis.py
+++ src/match_analysis.py
@@ -9,7 +9,7 @@ class MatchDataSummary:
         # simple properties
         self.duration = (self.data_frame['Time'].iloc[-1] - self.data_frame['Time'].iloc[0]) // 60
         self.round_count = self.data_frame.shape[0]
-        self.map_name = self.data_frame['Map'].iloc[0]
+        self.map_name = self.data_frame['Map'].iloc[-1]
 
         # to-be calculated properties
         self.rating1, self.ct_rating1, self.t_rating1 = 0, 0, 0
@@ -45,6 +45,22 @@ class MatchDataSummary:
         self.ct_kpr = self.calculate_kpr(ct_data)
         self.t_kpr = self.calculate_kpr(t_data)
 
+        # TODO: Kills, Assists, Survived Consistency Percentage
+
+        # Kill Death Ratio, Kill+Assist Death Ratio
+        self.kdr, self.kda = self.calculate_kdr_kda(self.data_frame)
+        self.ct_kdr, self.ct_kda = self.calculate_kdr_kda(ct_data)
+        self.t_kdr, self.t_kda = self.calculate_kdr_kda(t_data)
+
+        # Mean Equipment Value
+        self.mean_equip = int(round(self.data_frame['Current Equip. Value'].mean(skipna=True)))
+        self.ct_mean_equip = int((ct_data['Current Equip. Value'].mean(skipna=True)))
+        self.t_mean_equip = int((t_data['Current Equip. Value'].mean(skipna=True)))
+
+    @staticmethod
+    def calculate_rating(relevant_data_df):
+        pass
+
     @staticmethod
     def calculate_hsr(relevant_data_df):
         total_kills = relevant_data_df['Round Kills'].sum(skipna=True)
@@ -68,4 +84,16 @@ class MatchDataSummary:
             return 0
         else:
             return float(round(total_kills / total_rounds, 2))
+
+    @staticmethod
+    def calculate_kas(relevant_data_df):
+        pass
+
+    @staticmethod
+    def calculate_kdr_kda(relevant_data_df):
+        last_entry = relevant_data_df.iloc[-1]
+        kdr = float(round(last_entry['Kills'] / last_entry['Deaths'], 3))
+        kda = float(round((last_entry['Kills'] + last_entry['Assists']) / last_entry['Deaths'], 3))
+        return kdr, kda
+
 
