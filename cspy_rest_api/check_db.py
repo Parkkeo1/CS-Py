8619617624d51af3f9dd8c7e9cc9707b4f6b842c
Parkkeo1 <keonp2@illinois.edu>
2018-07-04 00:13:07 +0000

    added new py files for viewing db

---
+++ cspy_rest_api/check_db.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sqlite3
+
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+conn = sqlite3.connect('users_and_matches.db')
+all_matches_df = pd.read_sql('SELECT * FROM all_matches;', conn)
+all_users_df = pd.read_sql('SELECT * FROM all_users;', conn)
+
+print(all_users_df, file=open('all_data.txt', 'w'))
+print('\n', file=open('all_data.txt', 'a'))
+print(all_matches_df, file=open('all_data.txt', 'a'))
