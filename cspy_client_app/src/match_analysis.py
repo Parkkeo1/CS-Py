ea9104d8ce2cb079cb8f14787f19614de4a09c4c
Parkkeo1 <keonp2@illinois.edu>
2018-07-08 14:44:23 -0400

    implemented fix for kas and kdr/kda calculations. to-be tested.

--- cspy_client_app/src/match_analysis.py
+++ cspy_client_app/src/match_analysis.py
@@ -39,6 +39,8 @@ class MatchAnalysis:
         self.calculate_main()
 
     def calculate_main(self):
+        first_team = self.data_frame['Player Team'].iloc[0]
+
         ct_data = self.data_frame[self.data_frame['Player Team'] == 'CT']
         t_data = self.data_frame[self.data_frame['Player Team'] == 'T']
 
@@ -54,8 +56,8 @@ class MatchAnalysis:
             self.hsr = self.calculate_hsr(self.data_frame)
             self.mdc = self.calculate_mdc(self.data_frame)
             self.kpr = self.calculate_kpr(self.data_frame)
-            self.kas = self.calculate_kas(self.data_frame)
-            self.kdr, self.kda = self.calculate_kdr_kda(self.data_frame)
+            self.kas = self.calculate_kas(self.data_frame, 0, 0, 0)
+            self.kdr, self.kda = self.calculate_kdr_kda(self.data_frame, 0, 0, 0)
             self.mean_equip = int(round(self.data_frame['Current Equip. Value'].mean(skipna=True)))
 
         # CT
@@ -63,8 +65,16 @@ class MatchAnalysis:
             self.ct_hsr = self.calculate_hsr(ct_data)
             self.ct_mdc = self.calculate_mdc(ct_data)
             self.ct_kpr = self.calculate_kpr(ct_data)
-            self.ct_kas = self.calculate_kas(ct_data)
-            self.ct_kdr, self.ct_kda = self.calculate_kdr_kda(ct_data)
+
+            if first_team == 'CT':
+                self.ct_kas = self.calculate_kas(ct_data, 0, 0, 0)
+                self.ct_kdr, self.ct_kda = self.calculate_kdr_kda(ct_data, 0, 0, 0)
+            else:
+                self.ct_kas = self.calculate_kas(ct_data, t_data['Kills'].iloc[-1], t_data['Assists'].iloc[-1],
+                                                 t_data['Deaths'].iloc[-1])
+                self.ct_kdr, self.ct_kda = self.calculate_kdr_kda(ct_data, t_data['Kills'].iloc[-1],
+                                                                  t_data['Assists'].iloc[-1], t_data['Deaths'].iloc[-1])
+
             self.ct_mean_equip = int(round(ct_data['Current Equip. Value'].mean(skipna=True)))
 
         # T
@@ -72,8 +82,16 @@ class MatchAnalysis:
             self.t_hsr = self.calculate_hsr(t_data)
             self.t_mdc = self.calculate_mdc(t_data)
             self.t_kpr = self.calculate_kpr(t_data)
-            self.t_kas = self.calculate_kas(t_data)
-            self.t_kdr, self.t_kda = self.calculate_kdr_kda(t_data)
+
+            if first_team == 'T':
+                self.t_kas = self.calculate_kas(t_data, 0, 0, 0)
+                self.t_kdr, self.t_kda = self.calculate_kdr_kda(t_data, 0, 0, 0)
+            else:
+                self.t_kas = self.calculate_kas(t_data, ct_data['Kills'].iloc[-1], ct_data['Assists'].iloc[-1],
+                                                ct_data['Deaths'].iloc[-1])
+                self.t_kdr, self.t_kda = self.calculate_kdr_kda(t_data, ct_data['Kills'].iloc[-1],
+                                                                ct_data['Assists'].iloc[-1], ct_data['Deaths'].iloc[-1])
+
             self.t_mean_equip = int(round(t_data['Current Equip. Value'].mean(skipna=True)))
 
     def calculate_rating(self, relevant_data_df):
@@ -126,22 +144,11 @@ class MatchAnalysis:
             return float(round(total_kills / total_rounds, 2))
 
     @staticmethod
-    def calculate_kas(relevant_data_df):
+    def calculate_kas(relevant_data_df, prev_kill_count, prev_assist_count, prev_death_count):
         kas_count = 0
 
-        # Check first entry
-        if relevant_data_df['Round Kills'].iloc[0] > 0 or \
-           relevant_data_df['Kills'].iloc[0] > 0 or \
-           relevant_data_df['Assists'].iloc[0] > 0 or \
-           relevant_data_df['Deaths'].iloc[0] == 0:
-            kas_count += 1
-
-        prev_kill_count = relevant_data_df['Kills'].iloc[0]
-        prev_assist_count = relevant_data_df['Assists'].iloc[0]
-        prev_death_count = relevant_data_df['Deaths'].iloc[0]
-
         # Check rest of rounds
-        for i in range(1, relevant_data_df.shape[0]):
+        for i in range(0, relevant_data_df.shape[0]):
             if relevant_data_df['Round Kills'].iloc[i] > 0 or \
                relevant_data_df['Kills'].iloc[i] > prev_kill_count or \
                relevant_data_df['Assists'].iloc[i] > prev_assist_count or \
@@ -158,8 +165,13 @@ class MatchAnalysis:
         return round(kas_count / relevant_data_df.shape[0], 2) * 100
 
     @staticmethod
-    def calculate_kdr_kda(relevant_data_df):
+    def calculate_kdr_kda(relevant_data_df, prev_side_kills, prev_side_assists, prev_side_deaths):
         last_entry = relevant_data_df.iloc[-1]
-        kdr = float(round(last_entry['Kills'] / last_entry['Deaths'], 3))
-        kda = float(round((last_entry['Kills'] + last_entry['Assists']) / last_entry['Deaths'], 3))
+
+        kill_total = last_entry['Kills'] - prev_side_kills
+        assist_total = last_entry['Assists'] - prev_side_assists
+        death_total = last_entry['Deaths'] - prev_side_deaths
+
+        kdr = float(round(kill_total / death_total, 3))
+        kda = float(round((kill_total + assist_total) / death_total, 3))
         return kdr, kda
